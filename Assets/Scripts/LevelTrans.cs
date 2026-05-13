f4249567f912de04869015143c28361ce6c49141
gdtempleman-gif <gdtempleman@gmail.com>
2026-05-13 15:57:21 +0100

    Load scene on collision

--- Assets/Scripts/LevelTrans.cs
+++ Assets/Scripts/LevelTrans.cs
@@ -1,28 +1,14 @@
-using System.Collections.Generic;
-using System.Collections;
+
 using UnityEngine;
 using UnityEngine.SceneManagement;
 public class LevelTrans : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    public void ChangeScene(string sceneName)
     {
-        transform.Translate(transform.forward * Time.deltaTime * 2f);
+        SceneManager.LoadScene(sceneName);
     }
 
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.tag == "LevelExit")
-        {
-           // SceneManager.LoadScene(1);
-           SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-        }
-    }
 }
+
