30b6653688b5045dc53740c0cd1780f3c9c4288d
V0rtex <chukwukaofokansi@gmail.com>
2026-05-17 19:08:41 +0100

    new?

---
+++ Assets/Scripts/MenuSystem.cs
@@ -0,0 +1,34 @@
+using UnityEngine; //Greg
+using UnityEngine.SceneManagement;
+public class MenuSystem : MonoBehaviour
+{
+
+public void OnStartClick()
+    {
+        SceneManager.LoadScene("Tutorial");
+    }
+
+    public void OnCreditClick()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+
+    public void OnHelpClick()
+    {
+        SceneManager.LoadScene("Help");
+    }
+
+    public void OnReturnClick()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void OnExitClick()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+
+        Application.Quit();
+    }
+
+}
---
+++ ProjectSettings/Assets/Scripts/TheCheckPointScript.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class TheCheckPointScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    private TheRespawnScript Respawning;
+    private BoxCollider2D collide;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    void Awake()
+    {
+        collide = GetComponent<BoxCollider2D>();
+        Respawning = GameObject.FindGameObjectWithTag("Respawn").GetComponent<TheRespawnScript>();
+    }
+
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Respawning.respawnPoint = this.gameObject;
+            collide.enabled = false;
+        }
+    }
+}
