bcfe81ba639a85dbccda6b28d287d98ad21fc4e3
V0rtex <chukwukaofokansi@gmail.com>
2026-05-20 20:39:45 +0100

    PLEASE Work

---
+++ Assets/Art and Tiles/Scripts/LevelTrans.cs
@@ -0,0 +1,14 @@
+//Greg
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class LevelTrans : MonoBehaviour
+{
+
+    public void ChangeScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+
+}
+
